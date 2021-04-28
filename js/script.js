127a8812ebbb97731b0f0c48f9801bf8652b44fe
shrekt444 <andrea.maie444@gmail.com>
2021-04-28 14:58:36 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#root',
+    data: {     
+    },
+    methods: {
+    },
+    mounted() {
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((response) {
+
+            })
+    }
+}) 
